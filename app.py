de94189d211b1b0b1620009bb7e865d0e573d916
Hugo Ruscitti <hugoruscitti@gmail.com>
2012-05-18 17:42:08 -0300

    agregando link para visitar la documentacion del proyecto.

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ def index():
 def acerca_de():
     return render_template('acerca_de.html')
 
+@app.route('/documentacion')
+def documentacion():
+    return render_template('documentacion.html')
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
