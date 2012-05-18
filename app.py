e3482b9ef55453f51881b3623bf9a9fcda684c46
Hugo Ruscitti <hugoruscitti@gmail.com>
2012-05-18 17:29:14 -0300

    Creando la seccion acercade.

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/acerca_de')
+def acerca_de():
+    return render_template('acerca_de.html')
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
